6649415539fa1aaf8fcb908575a359ca894c6d6e
ririssilvia <64761595+ririssilvia@users.noreply.github.com>
2022-09-19 09:47:46 +0700

    Tugas 2 prespective

---
+++ lib/perspective.dart
@@ -0,0 +1,143 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+class MyPerspective extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Perspective',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key}) : super(key: key); // changed
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+  Offset _offset = Offset.zero; // changed
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Transform(
+        // Transform widget
+        transform: Matrix4.identity()
+          ..setEntry(3, 2, 0.001) // perspective
+          ..rotateX(0.01 * _offset.dy) // changed
+          ..rotateY(-0.01 * _offset.dx), // changed
+        alignment: FractionalOffset.center,
+        child: GestureDetector(
+          // new
+          onPanUpdate: (details) => setState(() => _offset += details.delta),
+          onDoubleTap: () => setState(() => _offset = Offset.zero),
+          child: _defaultApp(context),
+        ));
+  }
+
+  _defaultApp(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('The Matrix 3D'), // changed
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.displayMedium,
+            ),
+            Transform.rotate(
+              angle: -45 * (pi / 180.0),
+              child: ElevatedButton(
+                child: const Text("Rotated button"),
+                onPressed: () {},
+              ),
+            ),
+            Transform(
+              transform: Matrix4.rotationZ(-45 * (pi / 180.0)),
+              alignment: Alignment.center,
+              child: ElevatedButton(
+                child: const Text("Rotated button"),
+                onPressed: () {},
+              ),
+            ),
+            Transform.scale(
+              scale: 2.0,
+              child: ElevatedButton(
+                child: const Text("scaled up"),
+                onPressed: () {},
+              ),
+            ),
+            Transform(
+              transform: Matrix4.identity()..scale(2.0, 2.0),
+              alignment: Alignment.center,
+              child: ElevatedButton(
+                child: const Text("scaled up (matrix)"),
+                onPressed: () {},
+              ),
+            ),
+            Transform.translate(
+              offset: const Offset(100, 300),
+              child: ElevatedButton(
+                child: const Text("translated to bottom"),
+                onPressed: () {},
+              ),
+            ),
+            Transform(
+              transform: Matrix4.translationValues(100, 300, 0),
+              child: ElevatedButton(
+                child: const Text("translated to bottom (matrix)"),
+                onPressed: () {},
+              ),
+            ),
+            Transform.translate(
+              offset: const Offset(70, 200),
+              child: Transform.rotate(
+                angle: -45 * (pi / 180.0),
+                child: Transform.scale(
+                  scale: 2.0,
+                  child: ElevatedButton(
+                    child: const Text("multiple transformations"),
+                    onPressed: () {},
+                  ),
+                ),
+              ),
+            ),
+            Transform(
+              alignment: Alignment.center,
+              transform: Matrix4.translationValues(70, 200, 0)
+                ..rotateZ(-45 * (pi / 180.0))
+                ..scale(2.0, 2.0),
+              child: ElevatedButton(
+                child: const Text("multiple transformations (matrix)"),
+                onPressed: () {},
+              ),
+            )
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
